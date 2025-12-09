ce95b3bdf2ac6d57baeced90f54b7010d8703e8b
IbtsamGhreeb <ibrahimibtsam82@gmail.com>
2025-12-09 16:12:33 +0200

    Create 4.sp_task.sql

---
+++ Snowflake/4.sp_task.sql
@@ -0,0 +1,8 @@
+CREATE OR REPLACE TASK REFRESH_OLIST_METADATA_TASK
+  WAREHOUSE = snowflake_learning_wh
+  SCHEDULE = '15 MINUTE' 
+AS
+  CALL REFRESH_ICEBERG_METADATA_SP(
+    'olist.public.OLIST_TABLE', 
+    's3://unique-staging-bucket/BrazilianProject/'
+  );
